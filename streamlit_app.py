ab4aef8b90fd832ea099b1b44bb45a3cc73a9b32
manfredivale-glitch <manfredi.vale@gmail.com>
2026-05-26 13:53:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-# --- 1. MOTORE DI CALCOLO (Con VAN integrato) ---
+# --- 1. MOTORE DI CALCOLO ---
 def calcola_van(flussi_cassa, tasso_sconto=0.05):
-    """Calcola il Valore Attuale Netto dei flussi di cassa."""
     van = sum([flusso / ((1 + tasso_sconto) ** anno) for anno, flusso in enumerate(flussi_cassa, 1)])
     return van
 
@@ -13,7 +12,6 @@ def calcola_scenario(params, config):
     for anno in range(1, 6):
         som += 0.15
         
-        # Logica di calcolo
         riduzione_evap = 1.0 - (0.4 * params['agrivoltaico'] / 100)
         ricavo_energia_ha = 2200 * (params['agrivoltaico'] / 100)
         bonus = max(0, 500 * (1 - params['agrivoltaico']/100)) if params['biochar'] > 15 else 0
@@ -23,7 +21,6 @@ def calcola_scenario(params, config):
         risparmio_perm = fabbisogno_base * (params['permacultura'] / 100)
         fabbisogno_est = max(50, fabbisogno_base - (ritenzione * 1.5) - risparmio_perm)
         
-        # Risultato operativo (MOL)
         resa = 4.5 * min(config['risp_biochar'], ritenzione / 250)
         mol = (resa * config['prezzo']) + ricavo_energia_ha + bonus - config['costo_base'] - (fabbisogno_est * params['costo_h2o'])
         
@@ -42,10 +39,11 @@ st.title("🌱 Digital Twin: Progetto RAP - Val d'Agri")
 
 with st.sidebar:
     st.header("Parametri Progetto")
-    biochar = st.slider("Biochar (ton/ha)", 0, 30, 10)
-    agrivoltaico = st.slider("Agrivoltaico (%)", 0, 100, 20)
-    costo_h2o = st.slider("Costo Acqua (€/m3)", 0.1, 1.0, 0.45)
-    coltura = st.selectbox("Coltura", list(config_colture.keys()))
+    # Aggiunta la 'key' per evitare che i cursori scompaiano
+    biochar = st.slider("Biochar (ton/ha)", 0, 30, 10, key="bio")
+    agrivoltaico = st.slider("Agrivoltaico (%)", 0, 100, 20, key="agri")
+    costo_h2o = st.slider("Costo Acqua (€/m3)", 0.1, 1.0, 0.45, key="h2o")
+    coltura = st.selectbox("Coltura", list(config_colture.keys()), key="colt")
 
 # --- 4. ANALISI E KPI ---
 params_rap = {'biochar': biochar, 'agrivoltaico': agrivoltaico, 'costo_h2o': costo_h2o, 'permacultura': 20}
@@ -54,7 +52,6 @@ params_base = {'biochar': 0, 'agrivoltaico': 0, 'costo_h2o': costo_h2o, 'permacu
 df_rap = calcola_scenario(params_rap, config_colture[coltura])
 df_base = calcola_scenario(params_base, config_colture[coltura])
 
-# Calcolo VAN (assumendo tasso di sconto 5%)
 van_rap = calcola_van(df_rap['MOL_Euro'])
 van_base = calcola_van(df_base['MOL_Euro'])
 
